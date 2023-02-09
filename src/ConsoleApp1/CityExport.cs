e93e6189df05c7fd99b6ee9de435e1d45be87e26
Bayram Akbuz <bayram.akbuz@gmail.com>
2023-02-09 14:44:43 +0300

    turkiye_il_ilce.json dosyası eklendi

---
+++ src/ConsoleApp1/CityExport.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ConsoleApp1
+{
+    class CityExport
+    {
+        [JsonPropertyName("mame")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("district_count")]
+        public int DistrictCount { get; set; }
+
+        [JsonPropertyName("districts")]
+        public List<string> Districts { get; set; }
+    }
+}
---
+++ src/ConsoleApp1/CityRow.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ConsoleApp1
+{
+    class CityRow
+    {
+        public CityRow()
+        {
+
+        }
+
+        public CityRow(int startRow, string name, int districtCount)
+        {
+            StartRow = startRow;
+            Name = name;
+            DistrictCount = districtCount;
+        }
+
+        [JsonIgnore]
+        public int StartRow { get; set; }
+
+        [JsonIgnore]
+        public int EndRow { get; set; }
+
+        [JsonPropertyName("mame")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("district_count")]
+        public int DistrictCount { get; set; }
+
+        [JsonPropertyName("districts")]
+        public List<string> Districts { get; set; }
+    }
+}
